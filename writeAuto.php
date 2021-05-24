21644e7c620867a5f20a1f5bb307fc5fb7502cd2
Thien <thiennc1107@gmail.com>
2021-05-24 21:43:57 +0700

    I

--- writeAuto.php
+++ writeAuto.php
@@ -7,7 +7,7 @@
     $batden = $_POST["bden"];
     $tatden = $_POST["tden"];
     // update lai database
-    $sql = "update Timer set batdautuoi=$batdautuoi,ketthuctuoi=$ketthuctuoi,batden=$batden,tatden=$tatden";
+    $sql = "update Auto set batdautuoi=$batdautuoi,ketthuctuoi=$ketthuctuoi,batden=$batden,tatden=$tatden";
     mysqli_query($conn, $sql);
     mysqli_close($conn);
 
