5402e2765e6571c049d8c1ddc276b51dc914a310
Thien <thiennc1107@gmail.com>
2021-05-24 22:33:28 +0700

    i

--- writeAuto.php
+++ writeAuto.php
@@ -7,7 +7,7 @@
     $batden = $_POST["bden"];
     $tatden = $_POST["tden"];
     // update lai database
-    $sql = "UPDATE Auto SET `den_bat`=$batden,`den_tat`=$tatden,`tuoi_bat`=$batdautuoi,`tuoi_tat`=$ketthuctuoi WHERE 1";
+    $sql = "UPDATE Auto SET `den_bat`='12:00:00',`den_tat`='12:00:00',`tuoi_bat`='12:00:00',`tuoi_tat`='12:00:00' WHERE 1";
     mysqli_query($conn, $sql);
     mysqli_close($conn);
 
