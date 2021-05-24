d565652a08b7c1ca13c5578644035aa0c5802cde
Thien <thiennc1107@gmail.com>
2021-05-24 23:05:49 +0700

    i

--- writeManual.php
+++ writeManual.php
@@ -5,7 +5,7 @@
     $tuoi = $_POST["tuoi"];
     $den =$_POST["den"];
     // update lai database
-    $sql = "UPDATE `Manual` SET `Den`='$den',`Tuoi`='$den' WHERE 1";
+    $sql = "UPDATE `Manual` SET `Den`='$den',`Tuoi`='$tuoi' WHERE 1";
     mysqli_query($conn, $sql);
     mysqli_close($conn);
 
