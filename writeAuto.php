39111a8154e3ead94d61d4622fa3533dbe67da61
Thien <thiennc1107@gmail.com>
2021-05-24 22:31:50 +0700

    I

--- writeAuto.php
+++ writeAuto.php
@@ -6,10 +6,6 @@
     $ketthuctuoi = $_POST["kttuoi"];
     $batden = $_POST["bden"];
     $tatden = $_POST["tden"];
-    $batdautuoi=date("H:i:s",strtotime($batdautuoi));
-    $ketthuctuoi =date("H:i:s",strtotime($ketthuctuoi);
-    $batden=date("H:i:s",strtotime($batden));
-    $tatden=date("H:i:s",strtotime($tatden));
     // update lai database
     $sql = "UPDATE Auto SET `den_bat`=$batden,`den_tat`=$tatden,`tuoi_bat`=$batdautuoi,`tuoi_tat`=$ketthuctuoi WHERE 1";
     mysqli_query($conn, $sql);
